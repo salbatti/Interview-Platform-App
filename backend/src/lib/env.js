540c1047963d0b07589a8e555b40f15a2ac1d3a7
Steve George <98937566+salbatti@users.noreply.github.com>
2026-05-10 02:10:23 +0530

    Initial Commit

---
+++ backend/src/lib/env.js
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+
+
+dotenv.config()
+
+
+export const ENV ={
+    PORT : process.env.PORT,
+    db_url:process.env.DB_URL,
+}
---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { ENV } from "./lib/env.js";
+
+
+const app= express ();
+
+console.log(process.env.PORT);
+
+
+
+app.get("/",(req,res)=>{
+    res.status(200).json({msg:"success from api"})
+})
+
+
+
+app.listen(ENV.PORT,()=>{
+    console.log(`Server is running ${ENV.PORT}`);
+    
+})
