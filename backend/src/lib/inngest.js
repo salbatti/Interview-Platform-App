f389a7247f891f74bf18fd7192697853a4efc504
Steve George <98937566+salbatti@users.noreply.github.com>
2026-05-13 16:27:52 +0530

    inngest syntax updated

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -4,39 +4,45 @@ import User from "../models/User.js";
 
 export const inngest = new Inngest({ id: "talent-iq" });
 
-const syncUser =inngest.createFunction(
-    {id:"sync-user"},
-    {event:"clerk/user.created"},
-    async({event})=>{
-        await connectDB()
+const syncUser = inngest.createFunction(
+  {
+    id: "sync-user",
+    event: "clerk/user.created",
+  },
+  async ({ event }) => {
+    await connectDB();
 
-        const{id,email_addresses,first_name,last_name,image_url}=event.data
+    const {
+      id,
+      email_addresses,
+      first_name,
+      last_name,
+      image_url,
+    } = event.data;
 
+    const newUser = {
+      clerkId: id,
+      email: email_addresses[0]?.email_address,
+      name: `${first_name || ""} ${last_name || ""}`,
+      profileImage: image_url,
+    };
 
-        const newUser ={
-            clerkId:id,
-            email:email_addresses[0]?.email_addresses,
-            name:`${first_name || ""} ${last_name || ""}`,
-            profileImage:image_url
-        }
+    await User.create(newUser);
+  }
+);
 
-        await User.create(newUser)
-        //todo: do sth else
-    }
-)
+const deleteUserFromDB = inngest.createFunction(
+  {
+    id: "delete-user-from-db",
+    event: "clerk/user.deleted",
+  },
+  async ({ event }) => {
+    await connectDB();
 
-const deleteuserFromDB =inngest.createFunction(
-    {id:"delete-user-from-db"},
-    {event:"clerk/user.deleted"},
-    async({event})=>{
-        await connectDB()
+    const { id } = event.data;
 
-        const{id}=event.data
+    await User.deleteOne({ clerkId: id });
+  }
+);
 
-        await User.deleteOne({clerkId:id})
-
-        //todo: do sth else
-    }
-)
-
-export const functions=[syncUser,deleteuserFromDB]
+export const functions = [syncUser, deleteUserFromDB];
