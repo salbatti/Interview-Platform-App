973a3b5eba957c4a3b36ccb4bf191514c5534c08
Steve George <98937566+salbatti@users.noreply.github.com>
2026-05-15 01:10:14 +0530

    auth-db-sync1

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,9 @@ import express from "express";
 import { ENV } from "./lib/env.js";
 import path from "path";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
+import {serve} from "inngest/express"
+import { inngest, functions } from "./src/inngest"
 
 const app = express();
 
@@ -9,6 +12,14 @@ console.log(process.env.PORT);
 
 const __dirname = path.resolve()
 
+//middleware
+
+app.use(express.json())
+//credentials: true meaning ?? => server allows a browser to include cookies on request
+
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/c", (req, res) => {
     res.status(200).json({ msg: "c poiny" })
