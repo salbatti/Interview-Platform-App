8823ac47521c967284a2f68498e568148ab7cb00
Steve George <98937566+salbatti@users.noreply.github.com>
2026-05-15 10:55:22 +0530

    auth & stream  sync

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import User from "../models/User.js";
+import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 
 export const inngest = new Inngest({ id: "talent-iq" });
 
@@ -26,6 +27,12 @@ const syncUser = inngest.createFunction(
     };
 
     await User.create(newUser);
+
+    await upsertStreamUser({
+      id:newUser.clerkId.toString(),
+      name:newUser.name,
+      image:image_url,
+    })
   }
 );
 
@@ -38,6 +45,8 @@ const deleteUserFromDB = inngest.createFunction(
     const { id } = event.data;
 
     await User.deleteOne({ clerkId: id });
+
+    await deleteStreamUser(id.toString())
   }
 );
 
