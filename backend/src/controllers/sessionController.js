83b328cce7825c5f7cf987d898d82362a69bf712
Steve George <98937566+salbatti@users.noreply.github.com>
2026-05-27 04:57:24 +0530

    refactoring sessionController.js

--- backend/src/controllers/sessionController.js
+++ backend/src/controllers/sessionController.js
@@ -99,7 +99,15 @@ export async function joinSession(req, res) {
         //dount in taking instance and its sacving
         if (!session) return res.status(404).json({ message: "Session not found" })
 
-        if (session.participant) return res.status(404).json({ message: "Session is full" })
+        if (session.status !== "active") {
+            return res.status(400).json({ message: "Cannot join a completed session" });
+        }
+
+        if (session.host.toString() === userId.toString()) {
+            return res.status(400).json({ message: "Host cannot join their own session as participant" });
+        }
+
+        if (session.participant) return res.status(409).json({ message: "Session is full" })
 
         session.participant = userId
 
@@ -135,19 +143,18 @@ export async function endSession(req, res) {
             return res.status(404).json({ message: "Session already completed" })
         }
 
-        session.status === "completed"
-        await session.save()
-
         //doubt
         // delete stream video call
-        const call = streamClient.video.call("default",session.callId);
-        await call.delete({hard:true})
+        const call = streamClient.video.call("default", session.callId);
+        await call.delete({ hard: true })
 
         //doubt
         // delete stream chat channel
-        const channel = chatClient.channel("messaging",session.callId)
+        const channel = chatClient.channel("messaging", session.callId)
         await channel.delete()
 
+        session.status === "completed"
+        await session.save()
         res.status(200).json({ session, message: "Session ended successfully" })
 
     } catch (error) {
